f39c8efc29fbdd5c78d3932ae29b3d1aecf9c38b
bruno parente <brunoalex9@msn.com>
2023-12-14 12:38:35 +0000

    Update LockScreenPlugin.java

--- src/android/LockScreenPlugin.java
+++ src/android/LockScreenPlugin.java
@@ -1,5 +1,3 @@
-package org.apache.cordova.lockscreen;
-
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
@@ -10,18 +8,18 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.app.admin.DeviceAdminReceiver; // Import the DeviceAdminReceiver class
+import android.app.admin.DeviceAdminReceiver;
 
 public class LockScreenPlugin extends CordovaPlugin {
 
-    static final int ACTIVATION_REQUEST = 47; // identifies our request id
+    static final int ACTIVATION_REQUEST = 47;
     DevicePolicyManager devicePolicyManager;
     ComponentName deviceAdmin;
 
     @Override
     protected void pluginInitialize() {
         devicePolicyManager = (DevicePolicyManager) cordova.getActivity().getSystemService(Context.DEVICE_POLICY_SERVICE);
-        deviceAdmin = new ComponentName(cordova.getActivity(), DeviceAdminReceiver.class); // Use the default DeviceAdminReceiver
+        deviceAdmin = new ComponentName(cordova.getActivity(), DeviceAdminReceiver.class);
     }
 
     @Override
@@ -31,9 +29,9 @@ public class LockScreenPlugin extends CordovaPlugin {
                 Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
                 intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, deviceAdmin);
                 cordova.getActivity().startActivityForResult(intent, ACTIVATION_REQUEST);
-                // You should implement onActivityResult to check if the user enabled the Device Admin
             } else {
-                devicePolicyManager.lockNow();
+                // Lock the screen without turning it off
+                devicePolicyManager.lockNow(DevicePolicyManager.FLAG_DISMISS_KEYGUARD);
                 callbackContext.success();
             }
             return true;
