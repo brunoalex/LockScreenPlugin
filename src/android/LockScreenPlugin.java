6c0515998cd5762593ba4e6b2a5e194775567ab4
bruno parente <brunoalex9@msn.com>
2023-12-14 12:55:12 +0000

    Update LockScreenPlugin.java

--- src/android/LockScreenPlugin.java
+++ src/android/LockScreenPlugin.java
@@ -8,18 +8,18 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.app.admin.DeviceAdminReceiver;
+import android.app.admin.DeviceAdminReceiver; // Import the DeviceAdminReceiver class
 
 public class LockScreenPlugin extends CordovaPlugin {
 
-    static final int ACTIVATION_REQUEST = 47;
+    static final int ACTIVATION_REQUEST = 47; // identifies our request id
     DevicePolicyManager devicePolicyManager;
     ComponentName deviceAdmin;
 
     @Override
     protected void pluginInitialize() {
         devicePolicyManager = (DevicePolicyManager) cordova.getActivity().getSystemService(Context.DEVICE_POLICY_SERVICE);
-        deviceAdmin = new ComponentName(cordova.getActivity(), DeviceAdminReceiver.class);
+        deviceAdmin = new ComponentName(cordova.getActivity(), DeviceAdminReceiver.class); // Use the default DeviceAdminReceiver
     }
 
     @Override
@@ -29,9 +29,9 @@ public class LockScreenPlugin extends CordovaPlugin {
                 Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
                 intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, deviceAdmin);
                 cordova.getActivity().startActivityForResult(intent, ACTIVATION_REQUEST);
+                // You should implement onActivityResult to check if the user enabled the Device Admin
             } else {
-                // Lock the screen without turning it off
-                devicePolicyManager.lockNow(DevicePolicyManager.FLAG_DISMISS_KEYGUARD);
+                devicePolicyManager.lockNow();
                 callbackContext.success();
             }
             return true;
